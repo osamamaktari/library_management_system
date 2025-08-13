c496e6391090b9f8dfca71d756b619c1d02c56b0
samm <osamaaltal01@gmail.com>
2025-08-14 01:05:34 +0300

    Add(class):Book

--- src/LibrarySystem/Book.php
+++ src/LibrarySystem/Book.php
@@ -1,1 +1,38 @@
 <?php
+namespace LibrarySystem;
+
+class Book {
+    private $title;
+    private $author;
+    private $isAvailable;
+
+    public function __construct($title, $author) {
+        $this->title = $title;
+        $this->author = $author;
+        $this->isAvailable = true;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getAuthor() {
+        return $this->author;
+    }
+
+    public function isAvailable() {
+        return $this->isAvailable;
+    }
+
+    public function borrow() {
+        if ($this->isAvailable) {
+            $this->isAvailable = false;
+            return true;
+        }
+        return false;
+    }
+
+    public function return() {
+        $this->isAvailable = true;
+    }
+}
